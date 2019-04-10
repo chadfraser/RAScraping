c4c0445c990d073bf34f5211f2390ca9b7d809e3
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-04-10 04:42:43 -0600

    Initial commit

---
+++ RAScraping/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace RAScraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A");
+            var doc = LoadDocument("");
+            var htmlNodes = doc.DocumentNode.SelectNodes("//div[@class='trophyimage']//a");
+            foreach (var node in htmlNodes)
+            {
+                Console.WriteLine(node.Attributes["href"].Value);
+            }
+            Console.WriteLine("B");
+            Console.ReadLine();
+        }
+
+        static HtmlDocument LoadDocument(string url)
+        {
+            var website = new HtmlWeb();
+            HtmlDocument doc = website.Load(url);
+            return doc;
+        }
+    }
+}
