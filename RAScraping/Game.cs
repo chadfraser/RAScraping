6868ed2a4281423f0af0767dec5e4137c7b5e474
Chad Fraser <chad.fraser@mail.mcgill.ca>
2019-04-12 16:15:49 -0600

    Enable storing of data from scraped site

--- RAScraping/Game.cs
+++ RAScraping/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 
 /// <summary>
@@ -8,11 +9,18 @@ namespace RAScraping
 {
     public class Game
     {
+        private static string _baseUrl = "http://retroachievements.org";
+        public static string BaseUrl
+        {
+            get { return _baseUrl; }
+        }
+
         private string _name;
         private string _url;
+        private int _achievementCount;
         private int _totalPoints;
         private int _totalRetroRatioPoints;
-        private List<Achievement> _achievements;
+        //private List<Achievement> _achievements;
 
         public string Name
         {
@@ -29,24 +37,58 @@ namespace RAScraping
             get { return _totalPoints; }
             set { _totalPoints = value; }
         }
+        public int AchievementCount
+        {
+            get { return _achievementCount; }
+            set { _achievementCount = value; }
+        }
         public int TotalRetroRatioPoints
         {
             get { return _totalRetroRatioPoints; }
             set { _totalRetroRatioPoints = value; }
         }
-        public List<Achievement> Achievements
-        {
-            get { return _achievements; }
-            set { _achievements = value; }
-        }
+        //public List<Achievement> Achievements
+        //{
+        //    get { return _achievements; }
+        //    set { _achievements = value; }
+        //}
 
-
-        public Game(string name, string url)
+        public Game(string name, string urlSuffix)
         {
             this._name = name;
-            this._url = url;
-            _totalPoints = _totalRetroRatioPoints = 0;
-            _achievements = new List<Achievement>();
+            this._url = _baseUrl + urlSuffix;
+            _achievementCount = _totalPoints = _totalRetroRatioPoints = 0;
+            //_achievements = new List<Achievement>();
+        }
+        public Game(string urlSuffix) : this("", urlSuffix)
+        {
+        }
+
+        public void FillGameData(HtmlDocument doc)
+        {
+            HtmlNode nameNode = doc.DocumentNode.SelectSingleNode("//*[@class='longheader']");
+            HtmlNode retroPointsStringNode = doc.DocumentNode.SelectSingleNode("//*[@id='achievement']//*[@class='TrueRatio']");
+
+            HtmlNodeCollection boldTagNodes = doc.DocumentNode.SelectNodes("//*[@id='achievement']//b");
+
+
+            if (nameNode != null)
+            {
+                _name = nameNode.InnerText;
+            }
+            if (retroPointsStringNode != null)
+            {
+                var retroPointsString = retroPointsStringNode.InnerText;
+                retroPointsString = retroPointsString.Substring(1, retroPointsString.Length - 2);
+                Int32.TryParse(retroPointsString, out _totalRetroRatioPoints);
+            }
+            if (boldTagNodes != null && boldTagNodes.Count >= 7)
+            {
+                var achievementCountString = boldTagNodes[5].InnerText;
+                var totalPointsString = boldTagNodes[6].InnerText;
+                Int32.TryParse(achievementCountString, out _achievementCount);
+                Int32.TryParse(totalPointsString, out _totalPoints);
+            }
         }
     }
 }
--- RAScraping/Program.cs
+++ RAScraping/Program.cs
@@ -13,15 +13,31 @@ namespace RAScraping
     {
         static void Main(string[] args)
         {
-            using (StreamReader r = new StreamReader("usernames.json"))
+            RootObject usernames;
+            var users = new List<User>();
+
+            using (StreamReader r = new StreamReader("../../usernames.json"))
             {
                 var json = r.ReadToEnd();
-                var usernames = JsonConvert.DeserializeObject<RootObject>(json);
+                usernames = JsonConvert.DeserializeObject<RootObject>(json);
             }
 
-            var doc = LoadDocument("");
-            var links = GetLinks(doc);
+            foreach (string username in usernames.usernames)
+            {
+                var newUser = new User(username);
+                users.Add(newUser);
+                Console.WriteLine(newUser.Url);
 
+
+                HtmlDocument doc = LoadDocument(newUser.Url);
+                var completedGamesLinks = GetCompletedGames(doc);
+
+                //foreach (var url in completedGamesLinks)
+                //{
+                //    var newGame = 
+                //}
+            }
+            Console.ReadLine();
         }
 
         static HtmlDocument LoadDocument(string url)
@@ -31,7 +47,31 @@ namespace RAScraping
             return doc;
         }
 
-        static List<string> GetLinks(HtmlDocument doc)
+        static List<Game> GetCompletedGames(HtmlDocument doc)
+        {
+            var games = new List<Game>();
+            List<string> links = GetCompletedGameLinks(doc);
+
+            //var htmlNodes = doc.DocumentNode.SelectNodes("//div[@class='trophyimage']//a");
+            //if (htmlNodes == null)
+            //{
+            //    return null;
+            //}
+
+            foreach (var link in links)
+            {
+                var newGame = new Game(link);
+                games.Add(newGame);
+                var newDoc = LoadDocument(newGame.Url);
+
+                Console.WriteLine(newGame.Url);
+                newGame.FillGameData(newDoc);
+                System.Threading.Thread.Sleep(2000);
+            }
+            return games;
+        }
+
+        static List<string> GetCompletedGameLinks(HtmlDocument doc)
         {
             List<string> links = new List<string>();
 
@@ -43,14 +83,14 @@ namespace RAScraping
 
             foreach (var node in htmlNodes)
             {
-               links.Add(node.Attributes["href"].Value);
+                links.Add(node.Attributes["href"].Value);
             }
             return links;
         }
     }
+}
 
-    public class RootObject
-    {
+public class RootObject
+{
         public List<string> usernames { get; set; }
-    }
 }
